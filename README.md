f42285fbf2d196e56957bda3a830137027ae0ee8
Sam Kamperis <skamperis@gmail.com>
2024-09-22 18:46:50 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# MATH6016 Theorem Proving with Lean 2024-25
+
+This is the template codespace repository for Theorem Proving with Lean 2024-25.
+
+
---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "lean_programming" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «L» where
+  -- add any library configuration options here
